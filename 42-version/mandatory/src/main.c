926e93cd8645f11f581bd4e7ed093269998b2939
MrY2K <ayoubsamoraizi@gmail.com>
2025-10-01 18:07:55 +0100

    sync

--- 42-version/mandatory/src/main.c
+++ 42-version/mandatory/src/main.c
@@ -6,7 +6,26 @@
 /*   By: achoukri <achoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 17:54:13 by achoukri          #+#    #+#             */
-/*   Updated: 2025/09/26 17:54:14 by achoukri         ###   ########.fr       */
+/*   Updated: 2025/10/01 18:06:26 by achoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h> // for printf
+
+/*
+• open, close, read, write,
+printf, malloc, free, perror,
+strerror, exit, gettimeofday.
+
+• All functions of the math
+library (-lm man man 3 math).
+
+• gettimeofday()
+
+• All functions of the MinilibX library.
+*/
+
+int	main()
+{
+	printf("Hello There\n");
+}
